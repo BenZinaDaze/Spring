b61c3b473aadd4cdfa14720119c51645875c5f67
benz1 <o3325qq@gmail.com>
2023-02-24 17:04:34 +0800

    applicationcontext

---
+++ spring_ioc_test/src/test/java/com/test/ApplicationContextTest.java
@@ -0,0 +1,15 @@
+package com.test;
+
+import com.test.service.UserService;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class ApplicationContextTest {
+    public static void main(String[] args) {
+        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("applicationContext.xml");
+
+        UserService userService = (UserService) applicationContext.getBean("userService");
+        System.out.println(userService);
+
+    }
+}
